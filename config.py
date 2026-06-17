c200b2126f5c19e94a878fbc4036385509b3ce71
dimalaik234-gif <dimanaumov234@mail.ru>
2026-06-17 20:30:34 +0800

    Update and rename database.py to config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+BOT_TOKEN = "8816734888:AAG6gApnQMqt01gfkzM-O1-L43cFnBytdgk"
+
+# Словарь картинок (чистый синтаксис, без лишних JSON-блоков)
+IMAGES = {
+    "egg": "AgACAgIAAxkBAAEq0YRqMox4K8zQfyQifqRqoMKGgev3ZwACqxxrGxvakUmI9XL5NsIH6QEAAwIAA3kAAzwE",
+    "baby": "AgACAgIAAxkBAAEq0aNqMo0jsheksMYEJQ39I6zFTZwtsgACrhxrGxvakUn43UGRjwQftQEAAwIAA3kAAzwE",
+    "grows": "AgACAgIAAxkBAAEq0aVqMo0wnFxwwfdmaMWw7Zm_Gx59hAACrxxrGxvakUlzeA_YOnbpagEAAwIAA3kAAzwE",
+    "cat": "AgACAgIAAxkBAAEq0alqMo1EhfWa-4nz6J3QxJC0K1gK3wACsRxrGxvakUnoVJYEIjNrKQEAAwIAA3kAAzwE",
+    "error404": "AgACAgIAAxkBAAEq0atqMo1OKTzPwFA4d-d3lhljC5er5gACsxxrGxvakUniFL8DdWq9QAEAAwIAA3kAAzwE",
+    "sleep": "AgACAgIAAxkBAAEq0a1qMo1ZsbBGxylZ6IznSlpihaYENwACtBxrGxvakUlyUh4ldTkyFgEAAwIAA3kAAzwE",
+    "feed": "AgACAgIAAxkBAAEq0bFqMo1jJmt3Woadi3hX4h3RKzeyegACthxrGxvakUlmyXEsaNolxgEAAwIAA3kAAzwE"
+}
